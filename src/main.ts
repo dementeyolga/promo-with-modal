d70a217b92efebda67aecc53e5619752f4bbe0a9
dementeyolga <dementeyolya@gmail.com>
2024-06-25 12:41:34 +0300

    feat: add open and close modal functionality

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
+const modal = document.getElementById('modal');
+const tryButton = document.getElementById('tryButton');
+const closeModalButton = document.getElementById('closeModalButton');
 
+if (
+  tryButton instanceof HTMLButtonElement &&
+  modal instanceof HTMLDivElement &&
+  closeModalButton instanceof HTMLButtonElement
+) {
+  tryButton.addEventListener('click', () => {
+    modal.classList.remove('inactive');
+    modal.classList.add('active');
+  });
+
+  closeModalButton.addEventListener('click', () => {
+    modal.classList.remove('active');
+    modal.classList.add('inactive');
+  });
+}
